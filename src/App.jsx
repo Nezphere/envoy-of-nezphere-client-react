0902b326f4b6cd76494b4bd428cbf6631916fdde
Kailang Fu <kailangfu@gmail.com>
2018-07-03 00:52:14 -0700

    Add links to index

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+	render() {
+		return <div className="ta-c lh-2">
+			<h1 className="fz-2 mb-1">Envoy of Nezphere</h1>
+			<p>Are you looking for a fresh new journey?</p>
+			<p>Join us now at <strong className="fw-b">Nezphere</strong></p>
+			<ul>
+				<li>Join QQ群: <a href="https://jq.qq.com/?_wv=1027&k=5IVyjW2">797370703</a></li>
+				<li>Join Discord: <a href="https://discord.gg/mZxYkDB">Nezphere</a></li>
+			</ul>
+		</div>;
+	}
+}
+
+export default App;
